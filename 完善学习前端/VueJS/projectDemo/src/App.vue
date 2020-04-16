1adbe749f68d2688ce34e53673ef6fbe26d6f69b
bearbaba <2200522850@qq.com>
2020-04-16 23:07:52 +0800

    一个案例

---
+++ 完善学习前端/VueJS/projectDemo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="tar-bar">
+    <div>
+      <img src="../src/assets/img/tarBar/icon-test.svg" class="img">
+      <p>消息</p>
+    </div>
+    <div>
+      <img src="../src/assets/img/tarBar/icon-test_1.svg" alt="" class="img">
+      <p>购物车</p>
+    </div>
+    <div>
+      <img src="../src/assets/img/tarBar/icon-test_3.svg" class="img">
+      <p>分类</p>
+    </div>
+    <div>
+      <img src="../src/assets/img/tarBar/icon-test_2.svg" class="img">
+      <p>分享</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+
+<style scoped>
+@import "./assets/css/tarBar/tarBar.css"
+</style>
