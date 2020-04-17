35cf3793342fb27eff96189325635b0b1fe1732c
bearbaba <2200522850@qq.com>
2020-04-17 20:16:19 +0800

    vue案例组件化开发

--- 完善学习前端/VueJS/projectDemo/src/App.vue
+++ 完善学习前端/VueJS/projectDemo/src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-      <tar-bar></tar-bar>
+      <tar-bar>
+        <tar-bar-item class='tar-bar-item'>
+          <img src="./assets/img/tarBar/icon-test.svg" class="img">
+          <p>消息</p>
+        </tar-bar-item>
+        <tar-bar-item class='tar-bar-item'>
+          <img src="./assets/img/tarBar/icon-test_1.svg" alt="" class="img">
+          <p>购物车</p>
+        </tar-bar-item>
+        <tar-bar-item class='tar-bar-item'>
+          <img src="./assets/img/tarBar/icon-test_3.svg" class="img">
+          <p>分类</p>
+        </tar-bar-item>
+        <tar-bar-item class='tar-bar-item'>
+          <img src="./assets/img/tarBar/icon-test_2.svg" class="img">
+          <p>分享</p>
+        </tar-bar-item>
+      </tar-bar>
   </div>
 </template>
 
--- 完善学习前端/VueJS/projectDemo/src/components/tarBar/tarBar.vue
+++ 完善学习前端/VueJS/projectDemo/src/components/tarBar/tarBar.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="tar-bar">
-    <div>
-      <img src="../../assets/img/tarBar/icon-test.svg" class="img">
-      <p>消息</p>
-    </div>
-    <div>
-      <img src="../../assets/img/tarBar/icon-test_1.svg" alt="" class="img">
-      <p>购物车</p>
-    </div>
-    <div>
-      <img src="../../assets/img/tarBar/icon-test_3.svg" class="img">
-      <p>分类</p>
-    </div>
-    <div>
-      <img src="../../assets/img/tarBar/icon-test_2.svg" class="img">
-      <p>分享</p>
-    </div>
+    <slot></slot>
   </div>
 </template>
 
 <script>
+import tarBarItem from './tarBarItem';
+
 export default {
   name: 'TarBar',
+  components: {
+    tarBarItem,
+  },
 };
 </script>
 
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  @import "../../assets/css/tarBar/tarBar.css"
+  /* @import "../../assets/css/tarBar/tarBar.css" */
 </style>
+
