bf9722ba5f8482bbbccd63abbee4980c895a48f3
bearbaba <2200522850@qq.com>
2020-05-01 20:42:54 +0800

    webstorm缺少大量优秀插件，忍不了

---
+++ 完善学习前端/VueJS/项目案例/supercloth/src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="tar-bar">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBar"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 完善学习前端/VueJS/项目案例/supercloth/src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="tar-bar-item" @click="changeActive">
+    <div v-if="!isActive">
+      <slot name="item-img"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-img-active"></slot>
+    </div>
+    <div :style="activeTextColor">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    data(){
+      return {
+        isActive: false,
+      }
+    },
+    methods: {
+      changeActive(){
+        this.isActive=!this.isActive;
+      }
+    },
+    computed: {
+      // eslint-disable-next-line vue/return-in-computed-property
+      activeTextColor(){
+        if(this.isActive===true){
+          // eslint-disable-next-line no-undef
+          return {color:pink};
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
