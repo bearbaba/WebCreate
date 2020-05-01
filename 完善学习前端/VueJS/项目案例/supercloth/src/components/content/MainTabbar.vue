83ad810a9d26bef8c5fe9bf86a5e2f0ef5ac4b8e
bearbaba <2200522850@qq.com>
2020-05-01 17:26:05 +0800

    vue案例 vue主页tarbar的样式设置

--- 完善学习前端/VueJS/项目案例/supercloth/src/components/content/MainTabbar.vue
+++ 完善学习前端/VueJS/项目案例/supercloth/src/components/content/MainTabbar.vue
@@ -1,10 +1,21 @@
 <template>
-
+  <tab-bar>
+    <tab-bar-item></tab-bar-item>
+    <tab-bar-item></tab-bar-item>
+    <tab-bar-item></tab-bar-item>
+    <tab-bar-item></tab-bar-item>
+  </tab-bar>
 </template>
 
 <script>
+  import TabBar from "../common/tabbar/TabBar";
+  import TabBarItem from "../common/tabbar/TabBarItem";
   export default {
-    name: "MainTabbar"
+    name: "MainTabbar",
+    components: {
+      TabBar,
+      TabBarItem,
+    }
   }
 </script>
 
