e6086b9d75b079244e61a421ef04d94a0bb63795
bearbaba <2200522850@qq.com>
2020-05-06 00:07:21 +0800

    首页推荐用flex展示图片

--- 完善学习前端/VueJS/项目案例/supermarket/src/views/tabbar/home/childComps/RecommendView.vue
+++ 完善学习前端/VueJS/项目案例/supermarket/src/views/tabbar/home/childComps/RecommendView.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="recommend">
-
+    <div v-for="(item,index) in recommends" :key="index" class="recommend-image-box">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <p>{{item.title}}</p>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -19,5 +24,17 @@
 </script>
 
 <style scoped>
+  #recommend{
+    display: flex;
+    justify-content: space-around;
+  }
+  .recommend-image-box{
+    width:30%;
+    text-align: center;
+    padding-top:5px;
+}
+  .recommend-image-box>a>img{
 
+    height:60px;
+  }
 </style>
