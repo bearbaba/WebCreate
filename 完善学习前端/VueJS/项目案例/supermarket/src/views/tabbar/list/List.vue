0d88cf81c7d83568cda5b71ddd05590cd1513c3b
bearbaba <2200522850@qq.com>
2020-05-14 22:41:54 +0800

    better-scroll在项目中的使用

--- 完善学习前端/VueJS/项目案例/supermarket/src/views/tabbar/list/List.vue
+++ 完善学习前端/VueJS/项目案例/supermarket/src/views/tabbar/list/List.vue
@@ -1030,9 +1030,17 @@
       TopBar,
     },
     mounted() {
-        this.scroll = new BScroll(".wrapper", {});
-        console.log(this.scroll)
-        scroll.hasVerticalScroll=true;
+        this.scroll = new BScroll(".wrapper", {
+          probeType: 3,
+          pullUpLoad :true,
+        });
+        this.scroll.on('scroll', position=>{
+            console.log(position);
+          }
+        );
+        this.scroll.on('pullingUp', ()=>{
+          console.log("上拉加载更多");
+        })
     }
   }
 </script>
