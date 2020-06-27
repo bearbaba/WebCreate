afe46a075b19da1768353ca06521957f2bcba541
bearbaba <2200522850@qq.com>
2020-06-27 20:44:55 +0800

    算了，样式后期调

--- 完善学习前端/VueJS/项目案例/supermarket/src/views/detail/childComponents/DetailShopInfo.vue
+++ 完善学习前端/VueJS/项目案例/supermarket/src/views/detail/childComponents/DetailShopInfo.vue
@@ -6,9 +6,22 @@
     </div>
     <div class="price-data">
       <div class="price-data-number">
-        <div class="sale-number">{{shop.sells | shopCellsFormat}}</div>
+        <div class="sale-number number">{{shop.sells | shopCellsFormat}}</div>
+        <div class="sale-number-text">总销量</div>
+      </div>
+      <div class="price-treasure-data">
+        <div class="price-treasure-number number">{{shop.goodsCount}}</div>
+        <div class="price-treasure-text">全部宝贝</div>
       </div>
     </div>
+<!--    <div class="desripe-data">-->
+<!--      <table>-->
+<!--        <tr v-for="(item, index) in shop.score" :key="index">-->
+<!--          <td>{{item.name}}</td>-->
+<!--          <td :class="{"></td>-->
+<!--        </tr>-->
+<!--      </table>-->
+<!--    </div>-->
   </div>
 </template>
 
@@ -44,6 +57,7 @@
     display: flex;
     flex-wrap: nowrap;
     align-items: center;
+    margin: 20px 0;
   }
 .shop-logo{
   width: 4rem;
@@ -64,5 +78,27 @@
     font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
     font-size: 20px;
     color: #666666;
+  }
+
+  .price-data{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    text-align: center;
+    font-size: 18px;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  }
+  .price-data-number{
+
+  }
+
+  .price-treasure-data{
+    margin-top: 3px;
+    border-right: #837f7f 1px solid;
+    padding: 0 20px;
+  }
+
+  .number{
+    font-size: 20px;
   }
 </style>
